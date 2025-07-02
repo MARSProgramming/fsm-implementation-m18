adb87f14affdc99137b000246395353bbc9d1d05
paperclip <66579862+gitpaperclip@users.noreply.github.com>
2025-07-02 10:18:32 -0700

    initial commit

---
+++ marvinFSM/src/main/java/frc/robot/constants/Field.java
@@ -0,0 +1,223 @@
+package frc.robot.constants;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.*;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+
+@SuppressWarnings("UnusedVariable")
+public class Field {
+    public static final AprilTagFieldLayout FIELD_LAYOUT;
+
+    static {
+        FIELD_LAYOUT = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+        FIELD_LAYOUT.setOrigin(AprilTagFieldLayout.OriginPosition.kBlueAllianceWallRightSide);
+    }
+
+    public static final double FIELD_HEIGHT = 8.0518;
+    public static final double FIELD_LENGTH = 17.548249;
+
+    // April tag IDs
+    public static final int RED_LEFT_CORAL_STATION = 1;
+    public static final int RED_RIGHT_CORAL_STATION = 2;
+    public static final int RED_PROCESSOR = 3;
+    public static final int RED_RIGHT_NET = 4;
+    public static final int RED_LEFT_NET = 5;
+    public static final int RED_REEF_LEFT_DRIVER_STATION = 6;
+    public static final int RED_REEF_CENTER_DRIVER_STATION = 7;
+    public static final int RED_REEF_RIGHT_DRIVER_STATION = 8;
+    public static final int RED_REEF_RIGHT_BARGE = 9;
+    public static final int RED_REEF_CENTER_BARGE = 10;
+    public static final int RED_REEF_LEFT_BARGE = 11;
+    public static final int BLUE_RIGHT_CORAL_STATION = 12;
+    public static final int BLUE_LEFT_CORAL_STATION = 13;
+    public static final int BLUE_LEFT_BARGE = 14;
+    public static final int BLUE_RIGHT_BARGE = 15;
+    public static final int BLUE_PROCESSOR = 16;
+    public static final int BLUE_REEF_RIGHT_DRIVER_STATION = 17;
+    public static final int BLUE_REEF_CENTER_DRIVER_STATION = 18;
+    public static final int BLUE_REEF_LEFT_DRIVER_STATION = 19;
+    public static final int BLUE_REEF_RIGHT_BARGE = 20;
+    public static final int BLUE_REEF_CENTER_BARGE = 21;
+    public static final int BLUE_REEF_LEFT_BARGE = 22;
+
+    public static final Pose2d FAR_LEFT_STARTING_POSE_BLUE = new Pose2d(7.2, 7, Rotation2d.k180deg);
+    public static final Pose2d FAR_LEFT_STARTING_POSE_RED = new Pose2d(
+            FIELD_LENGTH - FAR_LEFT_STARTING_POSE_BLUE.getX(),
+            FIELD_HEIGHT - FAR_LEFT_STARTING_POSE_BLUE.getY(),
+            Rotation2d.kZero);
+    public static final Pose2d FAR_RIGHT_STARTING_POSE_BLUE = new Pose2d(
+            FAR_LEFT_STARTING_POSE_BLUE.getX(), FIELD_HEIGHT - FAR_LEFT_STARTING_POSE_BLUE.getY(), Rotation2d.k180deg);
+    public static final Pose2d FAR_RIGHT_STARTING_POSE_RED = new Pose2d(
+            FIELD_LENGTH - FAR_RIGHT_STARTING_POSE_BLUE.getX(),
+            FIELD_HEIGHT - FAR_RIGHT_STARTING_POSE_BLUE.getY(),
+            Rotation2d.kZero);
+
+    public static final Pose2d LEFT_STARTING_POSE_BLUE = new Pose2d(7.2, 6.14, Rotation2d.fromDegrees(45.0));
+    public static final Pose2d LEFT_STARTING_POSE_RED = new Pose2d(
+            FIELD_LENGTH - LEFT_STARTING_POSE_BLUE.getX(),
+            FIELD_HEIGHT - LEFT_STARTING_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(-135.0)); // todo red angles are wrong
+    public static final Pose2d RIGHT_STARTING_POSE_BLUE = new Pose2d(
+            LEFT_STARTING_POSE_BLUE.getX(),
+            FIELD_HEIGHT - LEFT_STARTING_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(-45.0));
+    public static final Pose2d RIGHT_STARTING_POSE_RED = new Pose2d(
+            FIELD_LENGTH - RIGHT_STARTING_POSE_BLUE.getX(),
+            FIELD_HEIGHT - RIGHT_STARTING_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(135.0)); // todo red angles are wrong
+
+    public static final Pose2d LEFT_STATION_PICKUP_POSE_BLUE = new Pose2d(0.6, 7.85, Rotation2d.fromDegrees(135));
+
+    public static final Pose2d LEFT_STATION_PICKUP_POSE_RED = new Pose2d(
+            FIELD_LENGTH - LEFT_STATION_PICKUP_POSE_BLUE.getX(),
+            FIELD_HEIGHT - LEFT_STATION_PICKUP_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(-55));
+    public static final Pose2d RIGHT_STATION_PICKUP_POSE_BLUE = new Pose2d(
+            LEFT_STATION_PICKUP_POSE_BLUE.getX(),
+            FIELD_HEIGHT - LEFT_STATION_PICKUP_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(-135));
+    public static final Pose2d RIGHT_STATION_PICKUP_POSE_RED = new Pose2d(
+            FIELD_LENGTH - RIGHT_STATION_PICKUP_POSE_BLUE.getX(),
+            FIELD_HEIGHT - RIGHT_STATION_PICKUP_POSE_BLUE.getY(),
+            Rotation2d.fromDegrees(55));
+
+    public static boolean isBlueAlliance() {
+        return DriverStation.getAlliance().orElse(DriverStation.Alliance.Blue) == DriverStation.Alliance.Blue;
+    }
+
+    public static Pose2d getFarLeftStartingPose(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? FAR_LEFT_STARTING_POSE_BLUE : FAR_LEFT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getFarRightStartingPose(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? FAR_RIGHT_STARTING_POSE_BLUE : FAR_RIGHT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getLeftStartingPose(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? LEFT_STARTING_POSE_BLUE : LEFT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getRightStartingPose(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? RIGHT_STARTING_POSE_BLUE : RIGHT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getLeftStationPickup(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? LEFT_STATION_PICKUP_POSE_BLUE : LEFT_STATION_PICKUP_POSE_RED;
+    }
+
+    public static Pose2d getRightStationPickup(DriverStation.Alliance alliance) {
+        return alliance == DriverStation.Alliance.Blue ? RIGHT_STATION_PICKUP_POSE_BLUE : RIGHT_STATION_PICKUP_POSE_RED;
+    }
+
+    public static Pose2d getFarLeftStartingPose() {
+        return isBlueAlliance() ? FAR_LEFT_STARTING_POSE_BLUE : FAR_LEFT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getFarRightStartingPose() {
+        return isBlueAlliance() ? FAR_RIGHT_STARTING_POSE_BLUE : FAR_RIGHT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getLeftStartingPose() {
+        return isBlueAlliance() ? LEFT_STARTING_POSE_BLUE : LEFT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getRightStartingPose() {
+        return isBlueAlliance() ? RIGHT_STARTING_POSE_BLUE : RIGHT_STARTING_POSE_RED;
+    }
+
+    public static Pose2d getLeftStationPickup() {
+        return isBlueAlliance() ? LEFT_STATION_PICKUP_POSE_BLUE : LEFT_STATION_PICKUP_POSE_RED;
+    }
+
+    public static Pose2d getRightStationPickup() {
+        return isBlueAlliance() ? RIGHT_STATION_PICKUP_POSE_BLUE : RIGHT_STATION_PICKUP_POSE_RED;
+    }
+
+    public static Pose3d getTagPose(int id) {
+        if (id < RED_LEFT_CORAL_STATION || id > BLUE_REEF_LEFT_BARGE) {
+            throw new IllegalArgumentException("id must be between 1 and 22");
+        }
+
+        return FIELD_LAYOUT.getTagPose(id).orElseThrow(() -> {
+            final String message = String.format("getTagPose called for unexpected tag %d", id);
+            return new RuntimeException(message);
+        });
+    }
+
+    public static Pose2d getDesiredFinalScoringPoseForCoral(
+            int tagID,
+            Constants.SuperstructureConstants.ScoringSide scoringSide,
+            Constants.SuperstructureConstants.ScoringLevel level) {
+        return getDesiredPointToDriveToForCoralScoring(tagID, scoringSide, level, 0.0);
+    }
+
+    public static Pose2d getDesiredPointToDriveToForCoralScoring(
+            int tagID,
+            Constants.SuperstructureConstants.ScoringSide scoringSide,
+            Constants.SuperstructureConstants.ScoringLevel level,
+            double distanceFromFinalScoringPose) {
+        
+        double xOffset = 0;
+        double yOffset = 0;
+        double rot = 90;
+        if (tagID >= 1 && tagID <= 22) {
+            Pose2d tagPose = Field.FIELD_LAYOUT.getTagPose(tagID).get().toPose2d();
+
+            if (scoringSide == Constants.SuperstructureConstants.ScoringSide.LEFT) {
+                if (level == Constants.SuperstructureConstants.ScoringLevel.L1) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_LEFT_L1_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_LEFT_L1_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L2) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_LEFT_L2_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_LEFT_L2_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L3) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_LEFT_L3_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_LEFT_L3_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L4) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_LEFT_L4_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_LEFT_L4_INCHES;
+                }         
+            }
+
+            if (scoringSide == Constants.SuperstructureConstants.ScoringSide.RIGHT) {
+                if (level == Constants.SuperstructureConstants.ScoringLevel.L1) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_RIGHT_L1_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_RIGHT_L1_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L2) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_RIGHT_L2_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_RIGHT_L2_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L3) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_RIGHT_L3_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_RIGHT_L3_INCHES;
+                }
+                else if (level == Constants.SuperstructureConstants.ScoringLevel.L4) {
+                    xOffset = Constants.SuperstructureConstants.X_OFFSET_RIGHT_L4_INCHES;
+                    yOffset = Constants.SuperstructureConstants.Y_OFFSET_RIGHT_L4_INCHES;
+                }         
+            }
+
+            xOffset = Units.inchesToMeters(xOffset + Units.metersToInches(distanceFromFinalScoringPose));
+            yOffset = Units.inchesToMeters(yOffset + Units.metersToInches(distanceFromFinalScoringPose));
+
+
+            if (scoringSide == Constants.SuperstructureConstants.ScoringSide.RIGHT) {
+                yOffset *= -1; // this needs to be provided, not inverted, the distances are Not the same
+            }
+            Translation2d offsetFromTag = new Translation2d(xOffset, yOffset);
+
+            var transformedPose =
+                    tagPose.transformBy(new Transform2d(offsetFromTag.getX(), offsetFromTag.getY(), Rotation2d.fromDegrees(rot)));
+
+            return transformedPose;
+        } else {
+            return Pose2d.kZero;
+        }
+    }
+}
---
+++ marvinFSM/src/main/java/frc/robot/constants/RobotMap.java
@@ -0,0 +1,61 @@
+package frc.robot.constants;
+
+import com.ctre.phoenix6.CANBus;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.units.Units;
+
+public class RobotMap {
+    public class DIO {
+      public static final int LIMIT = 0;
+    }
+
+    public class PWM {
+      public static final int SERVO = 4;
+    }
+
+    public class CAN {
+      public static final CANBus SECONDARY_BUS = new CANBus("CAN-2", "./logs/example.hoot");
+
+      public static final int CORAL = 12;
+      public static final int ELEVATOR_MASTER = 18;
+      public static final int ELEVATOR_FOLLOWER = 19;
+
+      public static final int PIGEON_LEFT = 20;
+      public static final int CANDLE = 40;
+
+      public static final int FRONT_LEFT_DRIVE = 2;
+      public static final int FRONT_LEFT_STEER = 3;
+      public static final int BACK_LEFT_DRIVE = 4;
+      public static final int BACK_LEFT_STEER = 5;
+      public static final int FRONT_RIGHT_DRIVE = 8;
+      public static final int FRONT_RIGHT_STEER = 9;
+      public static final int BACK_RIGHT_DRIVE = 6;
+      public static final int BACK_RIGHT_STEER = 7;
+    }
+
+    public class SPARK {
+      public static final int ALGAE = 11;
+    }
+
+    public class VISION {
+      public static final String reefCameraName = "reef_cam";
+      public static final String feederCameraName = "feeder_cam";
+
+      public static final Transform3d feederRobotToCam =
+          new Transform3d(
+              Units.Inches.of(-6),
+              Units.Inches.of(9.06),
+              Units.Inches.of(11.55),
+              new Rotation3d(Units.Degrees.of(0), Units.Degrees.of(0), Units.Degrees.of(125)));
+
+      public static final Transform3d reefRobotToCam =
+          new Transform3d(
+              Units.Inches.of(-6.3),
+              Units.Inches.of(9.49),
+              Units.Inches.of(13.44),
+              new Rotation3d(Units.Degrees.of(-10), Units.Degrees.of(0), Units.Degrees.of(90)));
+    }
+  
+}
